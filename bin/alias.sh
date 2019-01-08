50c4c1512ac1a05b876984a54bef91914ccd6848
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-01-08 09:34:00 +0100

    Add some docker aliases

--- bin/alias.sh
+++ bin/alias.sh
@@ -6,7 +6,7 @@ WORKSPACE=${WORKSPACE:-$HOME/Workspace}
 
 # System
 alias upg='sudo apt-get update && sudo apt-get upgrade -y && sudo apt autoremove'
-alias bat='upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|to\ full|percentage"'
+alias batt='upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|to\ full|percentage"'
 alias dockly='docker run -it -v /var/run/docker.sock:/var/run/docker.sock --rm --name dockly lirantal/dockly'
 alias dry='if [ $(docker ps | grep dry | wc -l) -eq 1 ]; then docker attach dry; else docker run -it -v /var/run/docker.sock:/var/run/docker.sock --rm --name dry moncho/dry; fi'
 
@@ -14,8 +14,10 @@ alias W='cd $WORKSPACE'
 
 # Docker
 alias d='docker'
-alias dr="docker run --rm -it"
-alias de="docker exec -it"
+alias dr='docker run --rm -it'
+alias de='docker exec -it'
+alias dps='docker ps'
+alias dpsa='docker ps -a'
 
 # Docker-Compose
 alias dc='docker-compose'
@@ -29,3 +31,4 @@ alias dcd!='docker-compose down -v --remove-orphans'
 
 alias composer='docker run --rm -it -v $(pwd):/app -w /app -u $(id -u):$(id -g) composer'
 
+alias va='vim $HOME/dotfiles/bin/alias.sh && source $HOME/dotfiles/bin/alias.sh'
